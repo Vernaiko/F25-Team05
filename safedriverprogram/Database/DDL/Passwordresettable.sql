767bbeaaa2c34a3459baa70765720f6a95fca555
Sean Farrell <spfarre@g.clemson.edu>
2025-09-18 16:08:57 -0400

    fixed commenting formatting for DDL files for users and passwords tokens

--- safedriverprogram/Database/DDL/Passwordresettable.sql
+++ safedriverprogram/Database/DDL/Passwordresettable.sql
@@ -1,3 +1,6 @@
+
+-- Password reset tokens table for forgot password functionality
+-- Essentially logs the password isn't stored and updated in this table
 CREATE TABLE password_reset_tokens (
     token_id INT AUTO_INCREMENT PRIMARY KEY,
     userID INT NOT NULL,
