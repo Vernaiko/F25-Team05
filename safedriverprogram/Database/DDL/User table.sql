014091b641e1b04320bd343871ccf3691c29d42c
Sean Farrell <spfarre@g.clemson.edu>
2025-09-18 16:55:57 -0400

    fixed size for password_hash
    fixed size for password_hash

--- safedriverprogram/Database/DDL/User table.sql
+++ safedriverprogram/Database/DDL/User table.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
     userID int(6) AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) NOT NULL UNIQUE,
     email VARCHAR(50) NOT NULL UNIQUE,
-    password_hash varchar(50) NOT NULL,
+    password_hash varchar(256) NOT NULL,
     first_name varchar(50),
     last_name varchar(50), 
     phone_number varchar(20),
