4e5439044fc6ec424d7e1bf8c19dbb01839afb15
Vernaiko <wlovin@clemson.edu>
2025-09-18 14:33:23 -0400

    Added Sign Up Page Front End
    Just like what I did before, this is merely the front end for the sign up page. Since the database isn't connected, I can't implement any of the logic for the page, but at least this should make it easier to test once we've gotten there!

--- safedriverprogram/driver_program/urls.py
+++ safedriverprogram/driver_program/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('login/', views.login_page, name='login'),
+    path('signup/', views.signup_page, name='signup'),
 ]
--- safedriverprogram/driver_program/views.py
+++ safedriverprogram/driver_program/views.py
@@ -6,3 +6,6 @@ def homepage(request):
 
 def login_page(request):
     return render(request, 'login.html')
+
+def signup_page(request):
+    return render(request, 'signup.html')
