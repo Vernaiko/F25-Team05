a00554806025355a0c0d811476c585b16ae98975
Vernaiko <wlovin@clemson.edu>
2025-10-09 13:49:50 -0400

    Minor update to settings.py
    minor update that should fix the server issues I am facing

--- safedriverprogram/safedriverprogram/settings.py
+++ safedriverprogram/safedriverprogram/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@5-pl@(mch*puojnu$_2c*8p%)3u#i@n$_1o#b_w$qus%#tq@$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '54.236.249.73']
 
 '''_hosts = os.getenv('DJANGO_ALLOWED_HOSTS', '')
 if _hosts:
