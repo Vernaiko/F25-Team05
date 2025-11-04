3948e787b7e39be979e966ba5cdbc370dbdf01e6
MillsGrims <144284199+MillsGrims@users.noreply.github.com>
2025-11-04 15:53:34 -0500

    update urls.py for remove button

--- safedriverprogram/driver_program/urls.py
+++ safedriverprogram/driver_program/urls.py
@@ -104,7 +104,8 @@ urlpatterns = [
 
     # Shopping cart
     path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-    path('cart/', views.view_cart, name='view_cart'),
+    path('cart/remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('cart/', views.view_cart, name='view_cart'),  
 
     # Reports and Account Review
     path('sponsor/driver-point-report/', views.generate_driver_point_report, name='generate_driver_point_report'),
